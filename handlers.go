62ff36fbe70e4896a8f6aa1e8f6ad48466a2620e
Nathan Heatwole <nathanheatwole@gmail.com>
2017-03-18 19:15:33 -0400

    rename and add model

--- handlers.go
+++ handlers.go
@@ -7,13 +7,13 @@ import (
 )
 
 func Index(writer http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(writer, "Hi there, I love %s!", request.URL.Path[1:])
+    fmt.Fprintf(writer, "Hi there, I love %s!\n", request.URL.Path[1:])
 }
 
 func Users(writer http.ResponseWriter, request *http.Request) {
-   fmt.Fprintf(writer, "%s", request.URL.Path[1:])
+   fmt.Fprintf(writer, "%s\n", request.URL.Path[1:])
 }
 
 func Locations(writer http.ResponseWriter, request *http.Request) {
-   fmt.Fprintf(writer, "%s", request.URL.Path[1:])
+   fmt.Fprintf(writer, "%s\n", request.URL.Path[1:])
 }
